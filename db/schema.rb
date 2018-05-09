5ce34f331009b78df53f3a2e1866e0d7164200bb
Vterebenin <niceserver11@mail.ru>
2018-05-09 18:35:54 +0300

    add categories and mixins

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_05_09_144657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "category"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
